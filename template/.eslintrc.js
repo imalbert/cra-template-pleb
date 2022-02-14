7e78d3b94d996bd3daaa0871a1d281f3d054db9b
imalbert <12537973+imalbert@users.noreply.github.com>
2022-02-14 08:50:17 +0800

    chore: update ts eslint plugin & parser

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   extends: [
     'react-app',
     'react-app/jest',
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
